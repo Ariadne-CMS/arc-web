2158b9e9b00a439f5dfcb8c1223c7a1832bf4e09
poef <auke@muze.nl>
2014-06-21 14:06:23 +0200

    fixed tests for installation via composer arc/arc

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,12 @@
 <?php
 
 	error_reporting(E_ALL|E_STRICT);
-	require_once( __DIR__.'/../autoload.php');
-//	require_once( __DIR__.'/vendor/autoload.php');	
+	include_once( __DIR__ . '/../../../autoload.php'); // arc/arc autoloader
+//	require_once( __DIR__ . '/../autoload.php');
+//	require_once( __DIR__ . '/vendor/autoload.php');	
 
-	require_once( __DIR__.'/vendor/lastcraft/simpletest/autorun.php');
-	require_once( __DIR__.'/vendor/lastcraft/simpletest/compatibility.php');
-	require_once( __DIR__.'/vendor/lastcraft/simpletest/browser.php');
-	require_once( __DIR__.'/vendor/lastcraft/simpletest/web_tester.php');
-	require_once( __DIR__.'/vendor/lastcraft/simpletest/unit_tester.php');
+	require_once( __DIR__ . '/vendor/lastcraft/simpletest/autorun.php');
+	require_once( __DIR__ . '/vendor/lastcraft/simpletest/compatibility.php');
+	require_once( __DIR__ . '/vendor/lastcraft/simpletest/browser.php');
+	require_once( __DIR__ . '/vendor/lastcraft/simpletest/web_tester.php');
+	require_once( __DIR__ . '/vendor/lastcraft/simpletest/unit_tester.php');
--- tests/test.url.php
+++ tests/test.url.php
@@ -11,7 +11,7 @@
 
      */
 
-    require_once( __DIR__.'bootstrap.php' );
+    require_once( __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php' );
      
     class TestUrl extends UnitTestCase {
 
