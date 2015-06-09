6911d0aa0322b938edcb9ddf9055695df7c184cf
Robbert Müller <spam.me@grols.ch>
2015-06-09 18:55:32 +0200

    Add unit tests

---
+++ tests/http.Test.php
@@ -0,0 +1,25 @@
+<?php
+
+    /*
+     * This file is part of the Ariadne Component Library.
+     *
+     * (c) Muze <info@muze.nl>
+     *
+     * For the full copyright and license information, please view the LICENSE
+     * file that was distributed with this source code.
+     */
+
+    class TestHTTP extends PHPUnit_Framework_TestCase
+    {
+        function testGet()
+        {
+            $res = \arc\http::get( 'http://www.ariadne-cms.org/', '?foo=bar' );
+            $this->assertNotEmpty($res);
+        }
+
+        function testClient()
+        {
+            $res = \arc\http::client();
+            $this->assertInstanceOf('\arc\http\ClientInterface',$res);
+        }
+    }
